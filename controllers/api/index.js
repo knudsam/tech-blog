0dcffa09ffd2cf9098f9a4128023dbe1fb54ee9e
knudsam <knudsam@gmail.com>
2023-07-19 18:49:42 -0400

    creating more js files within overall usage

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,8 @@ const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
 // Define the routes and their corresponding route files
-router.use('/user', userRoutes);       // Routes for user-related endpoints
-router.use('/post', postRoutes);       // Routes for post-related endpoints
-router.use('/comment', commentRoutes); // Routes for comment-related endpoints
+router.use('/user', userRoutes);   
+router.use('/post', postRoutes);     
+router.use('/comment', commentRoutes); 
 
 module.exports = router;
