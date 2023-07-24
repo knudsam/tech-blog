11b0bf7258b19ff56262ad286b2c944355cbf5a7
knudsam <knudsam@gmail.com>
2023-07-24 19:07:12 -0400

    updated models due to heroku error

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,48 +1,33 @@
 const postId = document.querySelector('input[name="post-id"]').value;
-console.log("testing");
-console.log(postId);
 
-// const id = window.location.toString().split('/')[
-//   window.location.toString().split('/').length - 1
-// ];
-
-const editFormHandler = async (event) => {
+const editFormHandler = async function(event) {
   event.preventDefault();
 
-  const postTitle = document.querySelector('input[name="post-title"]').value;
-  const postContent = document.querySelector('textarea[name="post-body"]').value;
+  const title = document.querySelector('input[name="post-title"]').value;
+  const body = document.querySelector('textarea[name="post-body"]').value;
 
-  console.log(postTitle);
-  console.log(postContent);
-
-  const response = await fetch(`/api/post/${postId}`, {
+  await fetch(`/api/post/${postId}`, {
     method: 'PUT',
     body: JSON.stringify({
-      postTitle,
-      postContent,
+      title,
+      body
     }),
     headers: {
       'Content-Type': 'application/json'
     }
   });
 
-  console.log(response);
-  if (response.ok) {
-    document.location.replace('/dashboard');
-  } else {
-    alert('Failed to update your post');
-  }
   document.location.replace('/dashboard');
 };
 
-const deleteClickHandler = async () => {
+const deleteClickHandler = async function() {
   await fetch(`/api/post/${postId}`, {
     method: 'DELETE'
   });
 
   document.location.replace('/dashboard');
 };
-// WHY ONE BUTTON IS SUBMIT AND THE OTHER IS CLICK?
+
 document
   .querySelector('#edit-post-form')
   .addEventListener('submit', editFormHandler);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,25 +1,21 @@
-const signupFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-input-signup').value.trim();
-    const password = document.querySelector('#password-input-signup').value.trim();
-    console.log(username);
-    console.log(password);
-    const response = await fetch('/api/user', {
-      method: 'POST',
-      body: JSON.stringify({
-        username, password
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to sign up');
-    }
-  };
-  
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+const newFormHandler = async function(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const body = document.querySelector('textarea[name="post-body"]').value;
+
+  await fetch(`/api/post`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      body,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  document.location.replace('/dashboard');
+};
+
+document
+  .querySelector('#new-post-form')
+  .addEventListener('submit', newFormHandler);
